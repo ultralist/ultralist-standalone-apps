775c607721a317b17e8a453730b1e822582a8e61
Grant Ammons <gammons@gmail.com>
2020-06-14 05:52:38 -0400

    update a bit

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 These are standalone native apps for Ultralist, built using [nativefier](https://github.com/jiahaog/nativefier).
 
-You can also build a native app on your own, if you have the `nativefier` library installed.  Check [`build.sh`](https://github.com/ultralist/ultralist-standalone-apps/blob/master/build.sh) for the specific commands.
+### Installing the pre-built binaries
 
-### Installation
+1. Grab the latest binary from the [releases page](https://github.com/ultralist/ultralist-standalone-apps/releases)
+2. Unzip + Install the app into a location that your OS can find.
 
-Copy the entire directory structure to your applications folder.  
+### Building on your own
+
+You can build this app on your own, rather than using the binaries on the release page.
+
+1. Ensure you have the `nativefier` library installed.
+2. Clone this repo.
+3. Run `build.sh`, or just run the `nativefier` command with the correct args for your arch. see [`build.sh`](https://github.com/ultralist/ultralist-standalone-apps/blob/master/build.sh) for the specific args.
+4. Install the app into a location that your OS can find.
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # build for linux
-nativefier --name "Ultralist" "app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p linux
+nativefier --name "Ultralist" "https://app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p linux
 zip -r ultralist-linux-x64.zip Ultralist-linux-x64
 
 # build for mac
-nativefier --name "Ultralist" "app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p osx
+nativefier --name "Ultralist" "https://app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p osx
 zip -r ultralist-darwin-x64.zip Ultralist-darwin-x64
 
 # build for windows
-nativefier --name "Ultralist" "app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p windows
+nativefier --name "Ultralist" "https://app.ultralist.io" --internal-urls ".*google\.com.*|ultralist\.io.*" -u "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/73.0" --app-copyright "Copyright (c) 2018-2020 Ultralist LLC.  All rights reserved" --icon "./icons/ultralist_logo.png" -p windows
 zip -r ultralist-win32-x64.zip Ultralist-win32-x64
